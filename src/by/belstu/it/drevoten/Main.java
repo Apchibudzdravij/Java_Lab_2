c466e9bb112b22ac951a1773b4b7eb586ed4bc70
Apchibudzdravij <drevoteny@gmail.com>
2022-02-15 12:26:03 +0300

    I debugged something

--- src/by/belstu/it/drevoten/Main.java
+++ src/by/belstu/it/drevoten/Main.java
@@ -13,5 +13,10 @@ public class Main
         for (int i=0; i<9; i++)
             if (i > -1 && i < 10)
                 System.out.println(i);
+
+        for (int counter = 0; counter < 29648; counter++)
+        {
+            System.out.println("Counter "+counter);
+        }
     }
 }
